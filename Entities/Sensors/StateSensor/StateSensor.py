b2a46ebe6e1bf87e78149a6591d29e0265dbea2e
infeeeee <gyetpet@mailbox.org>
2022-06-14 21:33:18 +0200

    Format and update StateSensor

--- Entities/Sensors/StateSensor/StateSensor.py
+++ Entities/Sensors/StateSensor/StateSensor.py
@@ -1,6 +1,6 @@
-import psutil
 from Entities.Entity import Entity
-import signal, sys
+import signal
+import sys
 import time
 
 TOPIC_STATE = 'state'
@@ -10,20 +10,20 @@ class StateSensor(Entity):
     def Initialize(self):
         self.AddTopic(TOPIC_STATE)
         signal.signal(signal.SIGINT, self.ExitSignal)
-
+        signal.signal(signal.SIGTERM, self.ExitSignal)
 
     def Update(self):
         self.SetTopicValue(TOPIC_STATE, self.consts.ONLINE_STATE)
 
     def SendOfflineState(self):
-        self.mqtt_client.SendTopicData(self.SelectTopic(TOPIC_STATE),self.consts.OFFLINE_STATE)
+        self.mqtt_client.SendTopicData(
+            self.SelectTopic(TOPIC_STATE), self.consts.OFFLINE_STATE)
 
-    def ExitSignal(self,sig, frame):
+    def ExitSignal(self, sig, frame):
         # Before exiting I send an offline message to the state_topic if prese
-        print("\r", end="") # This removes the Control-C symbol (^C)
-        self.Log(self.Logger.LOG_INFO,'Let me send the Offline state message')
+        print("\r", end="")  # This removes the Control-C symbol (^C)
+        self.Log(self.Logger.LOG_INFO, 'Let me send the Offline state message')
         self.SendOfflineState()
         time.sleep(1)
-        self.Log(self.Logger.LOG_INFO,"All done, goodbye !")
+        self.Log(self.Logger.LOG_INFO, "All done, goodbye !")
         sys.exit(0)
-        
