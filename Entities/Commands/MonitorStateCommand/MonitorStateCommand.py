d4da4b4be5eccde837704226267f18e6e6cf5fd2
infeeeee <gyetpet@mailbox.org>
2022-06-17 21:14:38 +0200

    Finished MonitorState switch

--- Entities/Commands/MonitorStateCommand/MonitorStateCommand.py
+++ Entities/Commands/MonitorStateCommand/MonitorStateCommand.py
@@ -5,12 +5,12 @@ from Entities.Entity import Entity
 from ctypes import *
 import re
 
-TOPIC = 'monitor_state'
+IN_TOPIC = 'monitor_state/set'
 
 
 class MonitorStateCommand(Entity):
     def Initialize(self):
-        self.SubscribeToTopic(TOPIC)
+        self.SubscribeToTopic(IN_TOPIC)
         self.stopCommand = False
         self.stopSensor = False
         self.stateOff = False
@@ -19,63 +19,68 @@ class MonitorStateCommand(Entity):
         os = self.GetOS()
 
         # Sensor function settings
-        if(os == self.consts.FIXED_VALUE_OS_WINDOWS):
-            self.GetMonitorState_OS = self.GetMonitorState_Win
-        elif(os == self.consts.FIXED_VALUE_OS_MACOS):
-            self.GetMonitorState_OS = self.GetMonitorState_macOS
-        elif(os == self.consts.FIXED_VALUE_OS_LINUX):
+        if(os == self.consts.FIXED_VALUE_OS_LINUX):
             self.GetMonitorState_OS = self.GetMonitorState_Linux
+        # elif(os == self.consts.FIXED_VALUE_OS_WINDOWS):
+        #     self.GetMonitorState_OS = self.GetMonitorState_Win
+        # elif(os == self.consts.FIXED_VALUE_OS_MACOS):
+        #     self.GetMonitorState_OS = self.GetMonitorState_macOS
         else:
             self.Log(self.Logger.LOG_WARNING,
                      'Monitor state is not available for this operating system')
             self.stopSensor = True
 
         # Command function settings
-        if(os == self.consts.FIXED_VALUE_OS_WINDOWS):
-            self.SetMonitorState_OS = self.SetMonitorState_Win
-        elif(os == self.consts.FIXED_VALUE_OS_MACOS):
-            self.SetMonitorState_OS = self.SetMonitorState_macOS
-        elif(os == self.consts.FIXED_VALUE_OS_LINUX):
+        if(os == self.consts.FIXED_VALUE_OS_LINUX):
             self.SetMonitorState_OS = self.SetMonitorState_Linux
+        # elif(os == self.consts.FIXED_VALUE_OS_WINDOWS):
+        #     self.SetMonitorState_OS = self.SetMonitorState_Win
+        # elif(os == self.consts.FIXED_VALUE_OS_MACOS):
+        #     self.SetMonitorState_OS = self.SetMonitorState_macOS
         else:
             self.Log(self.Logger.LOG_WARNING,
                      'No monitor state command available for this operating system')
             self.stopCommand = True
 
     def Callback(self, message):
-        state = message.payload.decode("utf-8")
+        state = message.payload.decode("utf-8").lower()
         if not self.stopCommand:
             try:
                 self.SetMonitorState_OS(state)
-            except ValueError:  # Not int -> not a message for that function
-                return
             except Exception as e:
                 raise Exception("Error during monitorstate set: " + str(e))
 
-            # Finally, tell the sensor to update and to send
-            self.CallUpdate()
+            # Need some timeout on linux before getting the state,
+            # so send state back manually:
+            if state == 'on':
+                send_state = self.consts.ON_STATE
+            else:
+                send_state = self.consts.OFF_STATE
+
+            self.mqtt_client.SendTopicData(
+                self.SelectTopic(self.STATE_TOPIC), send_state)
+
             self.lastSendingTime = None  # Force sensor to send immediately
 
     def Update(self):
         if not self.stopSensor:
-            self.SetTopicValue(TOPIC, self.GetMonitorState_OS(),
-                               self.ValueFormatter.TYPE_NONE)
+            self.SendOnlineState()
 
     def SetMonitorState_Linux(self, value):
         if sys_os.environ.get('DISPLAY'):
-            command = f'xset dpms force {value.lower()}'
+            command = f'xset dpms force {value}'
             subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         else:
             raise Exception(
                 'The Turn ON Monitors command is not available for this Linux Window System')
 
     def GetMonitorState_Linux(self):
-        command = 'xset q '
+        command = 'xset q'
         p = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         out = p.stdout.read().decode()
         p.communicate()
-        st = re.findall('Monitor is (.{2,3})', out)[0].upper()
-        return
+        st = re.findall('Monitor is (.{2,3})', out)[0].lower()
+        return st
 
     def GetOS(self):
         # Get OS from OsSensor and get temperature based on the os
@@ -90,9 +95,23 @@ class MonitorStateCommand(Entity):
         for data in discovery_data:
             data['expire_after'] = ""
 
+        self.SendOnlineState()
+
         discovery_data[0]['payload']['state_topic'] = self.SelectTopic(
-            TOPIC)
+            self.STATE_TOPIC)
+        discovery_data[0]['payload']['command_topic'] = self.SelectTopic(
+            IN_TOPIC)
         discovery_data[0]['payload']['payload_on'] = self.consts.ON_STATE
         discovery_data[0]['payload']['payload_off'] = self.consts.OFF_STATE
 
         return discovery_data
+
+    STATE_TOPIC = 'monitor_state/state'
+
+    def SendOnlineState(self, state=None):
+        if self.GetMonitorState_OS() == 'on':
+            self.mqtt_client.SendTopicData(
+                self.SelectTopic(self.STATE_TOPIC), self.consts.ON_STATE)
+        else:
+            self.mqtt_client.SendTopicData(
+                self.SelectTopic(self.STATE_TOPIC), self.consts.OFF_STATE)
